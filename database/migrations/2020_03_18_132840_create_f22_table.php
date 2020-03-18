2df184af6fbcae0948a06afd50acdfa8b5a9c7d1
platonovso <st.platonov@gmail.com>
2020-03-18 17:05:10 +0300

    seeds

---
+++ database/migrations/2020_03_18_132840_create_f22_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateF22Table extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('f22', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('f22');
+  }
+}
