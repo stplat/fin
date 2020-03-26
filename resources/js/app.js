0a836dbe69395d46ec5a086eaed3af203db87d15
platonovso <st.platonov@gmail.com>
2020-03-26 15:14:55 +0300

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -28,5 +28,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+  el: '#app',
+  data: {
+    message: 'Привет, Vue!'
+  }
 });
