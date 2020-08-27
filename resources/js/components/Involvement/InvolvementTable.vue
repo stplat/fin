9d045902e23b22028d8b1e3b20eb313d5f77f882
platonovso <st.platonov@gmail.com>
2020-08-27 16:48:16 +0300

    Раздел: Вовлечение

--- resources/js/components/Involvement/InvolvementTable.vue
+++ resources/js/components/Involvement/InvolvementTable.vue
@@ -19,6 +19,8 @@
       <template v-for="involvement in involvements">
         <tr class="strong bg">
           <td>{{ involvement.dkre }}</td>
+          <td>{{ addWithEmptyHelper([ involvement.total['involve_last'], involvement.total['involve_turnover'], involvement.total['involve_current'],
+            involvement.total['prepayment'] ]) | roundHelper }}</td>
           <td>{{ involvement.total['involve_last'] | roundHelper }}</td>
           <td>{{ involvement.total['involve_turnover'] | roundHelper }}</td>
           <td>{{ involvement.total['involve_current'] | roundHelper }}</td>
@@ -26,6 +28,8 @@
         </tr>
         <tr v-for="(activity, key) in involvement.activity" :key="involvement.dkre + key">
           <td>{{ activity.name }}</td>
+          <td>{{ addWithEmptyHelper([ activity['involve_last'], activity['involve_turnover'], activity['involve_current'],
+            activity['prepayment'] ]) | roundHelper }}</td>
           <td>{{ activity['involve_last'] | roundHelper }}</td>
           <td>{{ activity['involve_turnover'] | roundHelper }}</td>
           <td>{{ activity['involve_current'] | roundHelper }}</td>
